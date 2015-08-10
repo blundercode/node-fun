54802526a76092aa532d1b513a39e5012133c930
blundercode <erik.mcland@gmail.com>
2015-08-10 13:23:58 -0600

    wahtever

--- server.js
+++ server.js
@@ -3,9 +3,42 @@
 //#####################################################
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var cors = require('cors')
+var request = require('request');
+var twilio= require ('twilio')
 var app = express();
 var port = 3000;
+
+
+//#####################################################
+//Middelware:
+//#####################################################
+app.use(express.static('public'));
+
+// app.use(bodyParser.urlencoded({
+// 	extended: true
+// }));
+app.use(bodyParser.json());
+
+app.use(cors());
+
+var message = {
+	"message": "hello fucker"
+}
+//#####################################################
+//endPoints:
+//#####################################################
+
+app.get('/api/message', function(req, res) {
+	return res.json(message);
+});
+app.post('/api/send_text_message', function(req, res) {
+	console.log(req.body.message);
+	res.send();
+})
+
+
+
 
 app.listen(port, function() {
 	console.log('I\m watching you...', port);
