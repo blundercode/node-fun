1ddcbad2c0e3c8ab438e211eb22fb7eb4f6ca574
blundercode <erik.mcland@gmail.com>
2015-08-10 14:35:11 -0600

    final commit

--- server.js
+++ server.js
@@ -5,7 +5,15 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors')
 var request = require('request');
-var twilio= require ('twilio')
+var twilio = require('twilio')
+
+// Twilio Credentials 
+var accountSid = 'ACbb42949d459afbf800d030fafc3564fc';
+var authToken = '990602fb2f0c1152fd9a67d827c86d6c'; 
+ 
+//require the Twilio module and create a REST client 
+var client = twilio(accountSid, authToken);
+
 var app = express();
 var port = 3000;
 
@@ -29,18 +37,22 @@ var message = {
 //endPoints:
 //#####################################################
 
-app.get('/api/message', function(req, res) {
+app.get('/api/message', function (req, res) {
 	return res.json(message);
 });
-app.post('/api/send_text_message', function(req, res) {
+app.post('/api/send_text_message', function (req, res) {
 	console.log(req.body.message);
-	res.send();
-})
+	client.messages.create({
+		to: "14359947197",
+		from: "+14357740169",
+		body: req.body.message,
+	}, function (err, message) {
+		console.log(message.sid);
+	});
+});
 
 
-
-
-app.listen(port, function() {
+app.listen(port, function () {
 	console.log('I\m watching you...', port);
-})
+});
 
